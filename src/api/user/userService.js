5b62223e3ce01ad1690b79186942234ed20f9c50
tig <leehjpp4@gmail.com>
2026-05-07 11:37:51 +0900

    feat(user): 프로필 정보 입력 로직 추가

--- src/api/user/userService.js
+++ src/api/user/userService.js
@@ -6,6 +6,17 @@ async function getMe() {
     return response.data;
 }
 
+// 소셜 회원가입 유저 프로필 정보 입력
+async function setupProfile({ birthDate, address, phoneNumber }) {
+    const response = await axiosInstance.patch('/api/users/profile', {
+        birthDate,
+        address,
+        phoneNumber,
+    });
+    return response.data;
+}
+
 export const userService = {
     getMe,
+    setupProfile
 };

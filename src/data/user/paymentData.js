4858d65baebe0ae2f204805f570d1d61a40a978b
TIG-korea <leehjpp4@gmail.com>
2026-05-10 15:54:07 +0900

    feat(payment): enum 매핑 패키지

---
+++ src/data/user/paymentData.js
@@ -0,0 +1,37 @@
+export const PLAN_TYPE = {
+    monthly: 'MONTHLY',
+    yearly: 'YEARLY',
+};
+
+export const PLAN_AMOUNT = {
+    monthly: 9900,
+    yearly: 89000,
+};
+
+export const PG_MAP = {
+    card: 'html5_inicis',
+    kakao: 'kakaopay.TC0ONETIME',
+    bank: 'html5_inicis',
+};
+
+export const PAY_METHOD_MAP = {
+    card: 'card',
+    kakao: 'kakaopay',
+    bank: 'trans',
+};
+
+
+export const PAYMENT_METHOD_MAP = {
+    card: 'CARD',
+    kakao: 'KAKAOPAY',
+    bank: 'TRANSFER',
+};
+
+
+export const PLAN_NAME = {
+    monthly: '1개월 플랜',
+    yearly: '1년 플랜',
+};
+
+
+export const IMP_CODE = 'imp44032228';
